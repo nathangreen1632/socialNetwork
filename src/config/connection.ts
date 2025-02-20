b5f2080ecac2304b060ddd8bd704043faf776bd6
Nathan Green <ngreen1632@gmail.com>
2025-02-20 16:08:41 -0600

    Enhance MongoDB connection error handling and logging
    Added a more descriptive error message for missing MONGODB_URI to guide users toward checking their .env file. Also included a success log message upon establishing a MongoDB connection for better visibility.

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 if (!process.env.MONGODB_URI) {
-  throw new Error("MONGODB_URI environment variable is not defined");
+  throw new Error('MONGODB_URI environment variable is not defined. Please check your .env file.');
 }
 
 const MONGO_URI: string = process.env.MONGODB_URI;
@@ -12,7 +12,7 @@ const MONGO_URI: string = process.env.MONGODB_URI;
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-} as mongoose.ConnectOptions);
+} as mongoose.ConnectOptions).then(_r => console.log('Connected to MongoDB!'));
 
 const db = mongoose.connection;
 
