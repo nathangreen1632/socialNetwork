7acc5835e0d45d6e85b9321580de3a674aba2afc
Nathan Green <ngreen1632@gmail.com>
2025-02-21 14:55:47 -0600

    Refactor route imports and consolidate middleware.
    Replaced individual route imports with a centralized index file for cleaner and more maintainable routing. Updated middleware to use the consolidated routes under a single endpoint prefix.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import express, {Express} from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import thoughtRoutes from './routes/thoughtRoutes.js';
+import routes from "./routes/index.js";
 
 dotenv.config();
 
 const app: Express = express();
 
 app.use(express.json());
-app.use('/api/users', userRoutes);
-app.use('/api/thoughts', thoughtRoutes);
+app.use('api/', routes);
 
 mongoose.connect(process.env.MONGO_URI ?? 'mongodb://localhost:27017/socialNetwork_db').then(() => console.log('Connected to MongoDB!'));
 
