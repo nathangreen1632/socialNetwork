91360fcd10cdac0505704cb817da8167b567efc7
Nathan Green <ngreen1632@gmail.com>
2025-02-21 21:50:16 -0600

    Rename 'username' to 'name' and make 'createdAt' required.
    Updated the schema field from 'username' to 'name' for clarity and consistency. Additionally, marked the 'createdAt' field as required to ensure all reactions have a timestamp.

--- src/models/reactions.ts
+++ src/models/reactions.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const reactionSchema = new mongoose.Schema({
   reactionBody: { type: String, required: true },
-  username: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  name: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, required: true},
 });
 
 export default mongoose.model("Reaction", reactionSchema);
