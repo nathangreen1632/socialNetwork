b42c0998884b160e2efff9ac0ee2e26ce1de54e3
Nathan Green <ngreen1632@gmail.com>
2025-02-25 09:07:36 -0600

    Make "name" field optional in reaction schema.
    Updated the "name" field in the reaction schema to no longer be required. This change increases flexibility for use cases where a name is not mandatory.

--- src/models/reactions.ts
+++ src/models/reactions.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const reactionSchema = new mongoose.Schema({
   reactionBody: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: false },
   createdAt: { type: Date, default: Date.now, required: true},
 });
 
