5e98134ec34801b97e445698323d7901d19b1626
Nathan Green <ngreen1632@gmail.com>
2025-02-25 09:05:41 -0600

    Refactor router setup to remove unnecessary Express instance.
    Replaced the Express application with a direct Router instance to streamline routing logic. This simplifies the code by eliminating the unused Express app and focusing solely on the router configuration.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,14 @@
-import express, {Express, Router} from "express";
+import {Router} from "express";
 import userRoutes from "./userRoutes.js";
 import thoughtRoutes from "./thoughtRoutes.js";
 import reactionRoutes from "./reactionRoutes.js";
 import friendRoutes from "./friendRoutes.js";
 
-const app: Express = express();
-const router: Router = express.Router();
+const router: Router = Router();
 
-app.use('/users', userRoutes);
-app.use('/thoughts', thoughtRoutes);
-app.use('/reactions', reactionRoutes);
-app.use('/friends', friendRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/reactions', reactionRoutes);
+router.use('/friends', friendRoutes);
 
 export default router;
